cc7a71570a87804b342386ac38df430c88e9ef55
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-04-18 15:21:39 -0300

    Create questao16.c

---
+++ listaponteiros/questao16.c
@@ -0,0 +1,29 @@
+/*
+    Resposta:
+    aloha[2] = value funciona e atribui o valor 2.2 a terceira posição do vetor aloha
+    scanf("%f", &aloha) funciona e atribui um valor float à posição 0 do vetor aloha
+    aloha = "value" dá errado porque há atribuição de uma string (conjunto de char) a uma varíavel do tipo float
+    printf("%f", aloha) não dá erro de quebrar o código, mas não mostra o valor de aloha, pois não foi especificado nenhuma posição do vetor, irá mostrar lixo de memória
+    coisas[4][4] = aloha[3] funciona e atribui o valor do índice 3 de aloha à matriz de coisas na posição [4][4]
+    coisas[5] = aloha não funciona pois tenta acessar atribuir um vetor a uma array
+    pf = value não funciona, pois tenta atribuir um float a um ponteiro e não um endereço de memoria.
+    pf = aloha funciona por causa do isomorfismo entre os vetores/arrays e os ponteiros.
+*/
+
+#include <stdio.h>
+ 
+ int main(void) {
+    float aloha[10], coisas[10][5], *pf, value = 2.2;
+    int i=3;
+    
+    aloha[2] = value;
+    scanf("%f", &aloha);  // apresenta warnings mas funciona
+    // aloha = "value";
+    printf("%f", aloha);  // apresenta warnings mas não quebra o código
+    coisas[4][4] = aloha[3];
+    // coisas[5] = aloha;
+    // pf = value;
+    pf = aloha;
+    
+    return 0;
+ }
