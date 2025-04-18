faebf2a7d36893eaa6a4296624f02645b77afb96
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-04-18 18:02:51 -0300

    Update [incompleta]questao19.c

--- listaponteiros/[incompleta]questao19.c
+++ listaponteiros/[incompleta]questao19.c
@@ -3,8 +3,23 @@
 #include <string.h>
 
 int main(void){
-    float n, *vet;
-    printf("Digite a quantidade de floats que você deseja inserir");
+    int n;
+    float *vet;
+    
+    printf("Digite a quantidade de floats que você deseja inserir:\n");
+    scanf("%d", &n);
+    
+    // Alocação da memória
+    vet = malloc(n*sizeof(float));
+    
+    for(int i=0; i<n; i+=1){
+        scanf("%f", &vet[i]);
+    }
+    
+    
+    for(int i=0; i<n; i+=1){
+        printf("%f ", vet[i]);
+    }
     
     return 0;
 }
