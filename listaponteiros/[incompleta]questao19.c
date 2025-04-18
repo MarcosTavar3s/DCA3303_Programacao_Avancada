a2143608a72ce3c88ced770401cc0ab8b26cf690
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-04-18 17:57:38 -0300

    Create [incompleta]questao19.c

---
+++ listaponteiros/[incompleta]questao19.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+    float n, *vet;
+    printf("Digite a quantidade de floats que você deseja inserir");
+    
+    return 0;
+}
