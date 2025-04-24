78a9d74ed1c3d5ce72e2560dddbd239f682c668d
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-04-23 21:36:00 -0300

    Update questao16.c

--- listaponteiros/questao16.c
+++ listaponteiros/questao16.c
@@ -1,7 +1,7 @@
 /*
     Resposta:
     aloha[2] = value funciona e atribui o valor 2.2 a terceira posição do vetor aloha
-    scanf("%f", &aloha) funciona e atribui um valor float à posição 0 do vetor aloha
+    scanf("%f", &aloha) não funciona, pois tenta atribui um valor float à posição do vetor aloha (endereço)
     aloha = "value" dá errado porque há atribuição de uma string (conjunto de char) a uma varíavel do tipo float
     printf("%f", aloha) não dá erro de quebrar o código, mas não mostra o valor de aloha, pois não foi especificado nenhuma posição do vetor, irá mostrar lixo de memória
     coisas[4][4] = aloha[3] funciona e atribui o valor do índice 3 de aloha à matriz de coisas na posição [4][4]
@@ -17,7 +17,7 @@
     int i=3;
     
     aloha[2] = value;
-    scanf("%f", &aloha);  // apresenta warnings mas funciona
+    scanf("%f", &aloha);  // apresenta warnings mas funciona por sorte
     // aloha = "value";
     printf("%f", aloha);  // apresenta warnings mas não quebra o código
     coisas[4][4] = aloha[3];
