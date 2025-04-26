437e6b486a937c1c7285a693e21df7e3cdfc5b2b
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-04-26 15:11:11 -0300

    Update questao30.c

--- listaponteiros/questao30.c
+++ listaponteiros/questao30.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprimir(int ***matriz, int np, int nl, int nc){
+void imprimir(int ***matriz, int np, int nl, int nc, int plano){
+    // decremento porque o usuario nao começa a contar do 0
+    --plano;
+    
+    printf("Plano %d:\n", plano+1);
+    if(nc >= plano){
+        for (int j = 0; j < nl; ++j) {
+            for (int k = 0; k < nc; ++k) {
+                printf("%d ", matriz[plano][j][k]);
+            }
+                printf("\n");
+        }
+        return;
+    }
+    
      for(int i = 0; i < np; ++i) {
         printf("\nPlano %d:\n", i+1);
         for (int j = 0; j < nl; ++j) {
@@ -62,8 +76,11 @@ int main() {
     scanf("%d", &impressao);
     
     if(impressao==1){
+        int plano;
+        printf("Qual plano voce deseja imprimir(comecando de 1)?\n Se quiser todos, digite uma dimensao maior que o numero de planos disponiveis\n");
+        scanf("%d", &plano);
         // impressão
-        imprimir(matriz, np, nl, nc);
+        imprimir(matriz, np, nl, nc, plano);
     }
     
     printf("Deseja mudar algum valor?\nDigite 1 para sim e 0 para nao\n");
@@ -72,7 +89,7 @@ int main() {
     if(modificar_arr==1){
         // dimensão da array que vai ser alterada
         int nnp, nnl, nnc;
-        printf("Digite o plano, linha e coluna:\n");
+        printf("Digite o plano, linha e coluna (comecando de 1):\n");
         scanf("%d %d %d", &nnp, &nnl, &nnc);
         
         if(nnp > np+1 || nnl > nl+1 || nnc > nc+1){
@@ -84,7 +101,7 @@ int main() {
         // -1 pois o usuario não dá input com o plano começando com 0
         scanf("%d", &matriz[nnp-1][nnl-1][nnc-1]);
         
-        imprimir(matriz, np, nl, nc); // imprime depois de alterado
+        imprimir(matriz, np, nl, nc, nnp); // imprime o plano depois de alterado
 
     }
     
