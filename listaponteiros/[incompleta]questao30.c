00e70eda23ddc16fa50afb770207034a36170dc8
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-04-26 14:37:52 -0300

    Update [incompleta]questao30.c

--- listaponteiros/[incompleta]questao30.c
+++ listaponteiros/[incompleta]questao30.c
@@ -2,26 +2,66 @@
 #include <stdlib.h>
 
 int main() {
-    // declaracao de variaveis
-    int ***matriz, np, nl, nc;
-    
+    int ***matriz;
+    int np, nl, nc;
+
     printf("Coloque o numero de planos, linhas e colunas (profundidade, altura e comprimento):\n");
     scanf("%d %d %d", &np, &nl, &nc);
     
-    // aloca número de planos
+    // alocação da matriz
     matriz = malloc(np * sizeof(int**));
-    
-    // aloca espaço para todas as linhas
     matriz[0] = malloc(np * nl * sizeof(int*));
-    
-    // aloca espaço para todos os elementos
     matriz[0][0] = malloc(np * nl * nc * sizeof(int));
     
+    /*
+        planos -> int ***n -> plano1    plano2      plano3
+        linhas -> int **n ->  linha1    linha2      linha3
+        colunas -> int *n -> coluna1    coluna2    coluna3
+    */
     
-    // liberação do espaço alocado
+    // fixação dos planos
+    for(int i = 1; i < np; ++i) {
+        matriz[i] = matriz[i-1] + nl;
+    }
+    
+    // fixação das linhas em relação ao plano
+    for (int i = 1; i < np; ++i) {
+        matriz[i][0] = matriz[i-1][0] + (nl * nc);
+    }
+    
+    // fixação das colunas
+    for (int i = 0; i < np; ++i) {
+        for (int j = 1; j < nl; ++j) {
+            matriz[i][j] = matriz[i][j-1] + nc;
+        }
+    }
+
+    // preenchimento
+    for(int i = 0; i < np; ++i) {
+        for (int j = 0; j < nl; ++j) {
+            for (int k = 0; k < nc; ++k) {
+                matriz[i][j][k] = rand() % 10;
+            }
+        }
+    }
+
+    
+    // impressão
+    for(int i = 0; i < np; ++i) {
+        printf("\nPlano %d:\n", i);
+        for (int j = 0; j < nl; ++j) {
+            for (int k = 0; k < nc; ++k) {
+                printf("%d ", matriz[i][j][k]);
+            }
+            printf("\n");
+        }
+    }
+    
+    
+    // liberação da memoria
     free(matriz[0][0]);
     free(matriz[0]);
     free(matriz);
-    
+
     return 0;
 }
