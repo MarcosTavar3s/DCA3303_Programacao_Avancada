b1f8c386b3fabc1a9e272e2a487db947e6d3695c
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-06-04 17:01:29 -0300

    definicao do projeto: pinguim

--- sculptor_digital/main.cpp
+++ sculptor_digital/main.cpp
@@ -1,21 +1,38 @@
-#include <iostream>
 #include "sculptor.h"
 
 int main(void)
 {
-    std::cout<<"teste\n";
+    // cria um escultor cuja matriz tem 50x50x50 voxels
+    Sculptor pinguim(50, 50, 50);
 
-    // cria um escultor cuja matriz tem 10x10x10 voxels
-    Sculptor trono(10,10,10);
-    // para mudar a cor do voxel
-    trono.setColor(0, 1.0, 1.0, 1.0); // azul
-    // ativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
-    // trono.putEllipsoid(2, 2, 2, 2, 3, 4);
-    // trono.putBox(0, 9, 0, 9, 0, 9);
-    trono.putSphere(5, 5, 5, 3);
-    // desativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
-    // trono.cutBox(1, 8, 1, 9, 1, 9);
-    // grava a escultura digital no arquivo "trono.off"
-    trono.writeOFF("trono.off");
-    std::cout<<"Passou\n";
+    // cabeca do pinguim
+    pinguim.setColor(0.2, 0.2, 0.2, 1.0);
+    // insere esfera
+    pinguim.putSphere(40, 40, 40, 7);
+
+    // corta voxels extremos que ressaltam a cabeça do pinguim
+    pinguim.cutVoxel(47, 40, 40);
+    pinguim.cutVoxel(40, 47, 40);
+    pinguim.cutVoxel(40, 40, 47);
+    pinguim.cutVoxel(33, 40, 40);
+    pinguim.cutVoxel(40, 33, 40);
+    pinguim.cutVoxel(40, 40, 33);
+
+    pinguim.setColor(1.0, 1.0, 1.0, 1.0);
+
+    pinguim.cutSphere(37, 40, 40, 5);
+    pinguim.putSphere(37, 40, 40, 5);
+    pinguim.cutBox(32, 35, 35, 45, 35, 45);
+        // // para mudar a cor do voxel
+        // trono.setColor(0, 0.5, 1.0, 1.0); // azul
+        // // ativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
+        // trono.putEllipsoid(6, 5, 5, 3, 4, 3);
+        // // trono.putBox(0, 9, 0, 9, 0, 9);
+        // // trono.putSphere(50, 50, 50, 30);
+        // // trono.cutSphere(6, 6, 6, 2);
+        // // desativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
+        // // trono.cutBox(1, 8, 1, 9, 1, 9);
+        // // grava a escultura digital no arquivo "trono.off"
+        // trono.writeOFF("trono.off");
+    pinguim.writeOFF("pinguim.off");
 }
